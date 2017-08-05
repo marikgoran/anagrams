5d59a18e1c6e0c1b025b4238cedc538984624411
Goran Marik <goran@T460s>
2017-08-05 00:15:55 -0400

    find all permutations, with duplicates

--- main.go
+++ main.go
@@ -32,9 +32,40 @@ func anagrams (w http.ResponseWriter, r *http.Request) {
 	}
 
 	a.Word = word
-	a.Anagrams = []string{"A","B",word}
+	a.Anagrams = getPerms(word)
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(w).Encode(a)
 }
 
+func getPerms(str string) []string {
+	// base case, for one char, all perms are [char]
+	if len(str) == 1 {
+		return []string{str}
+	}
+
+	current := str[0:1] // current char
+	remStr := str[1:] // remaining string
+
+	perms := getPerms(remStr) // get perms for remaining string
+
+	allPerms := make([]string, 0) // array to hold all perms of the string based on perms of substring
+
+	// for every perm in the perms of substring
+	for _, perm := range perms {
+	        // add current char at every possible position
+		for i := 0; i <= len(perm); i++ {
+			newPerm := insertAt(i, current, perm)
+			allPerms = append(allPerms, newPerm)
+		}
+	}
+
+	return allPerms
+}
+
+// Insert a char in a word
+func insertAt(i int, char string, perm string) string {
+	start := perm[0:i]
+	end := perm[i:len(perm)]
+	return start + char + end
+}
